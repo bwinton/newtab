63e3f9216c8c4436cd692dc137f177dae27ab154
Blake Winton <bwinton@latte.ca>
2013-04-17 15:36:02 -0400

    Load in searchURLs, so that we can actually search.

--- scripts/base.js
+++ scripts/base.js
@@ -111,16 +111,15 @@ $(function () {
   };
 
   /* Set the default search engine. */
-  setSearch({'name': 'Google', 'image': 'google'}, [
-    {'name': 'Demo Google', 'image': 'google'},
-    {'name': 'Demo Yahoo', 'image': 'yahoo'},
-    {'name': 'Demo Bing', 'image': 'bing'},
-    {'name': 'Demo Amazon.com', 'image': 'amazon'},
-    {'name': 'Demo eBay', 'image': 'ebay'},
-    {'name': 'Demo Twitter', 'image': 'twitter'},
-    {'name': 'Demo Wikipedia (en)', 'image': 'wikipedia'}
+  setSearch({'name': 'Google', 'image': 'google', 'searchURL': 'https://www.google.com/search?q=_searchTerms_&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:en-US:unofficial&client=firefox-a&channel=np&source=hp'}, [
+    {'name': 'Demo Google', 'image': 'google', 'searchURL': 'https://www.google.com/search?q=_searchTerms_&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:en-US:unofficial&client=firefox-a&channel=np&source=hp'},
+    {'name': 'Demo Yahoo', 'image': 'yahoo', 'searchURL': 'http://search.yahoo.com/search?p=_searchTerms_&ei=UTF-8&fr='},
+    {'name': 'Demo Bing', 'image': 'bing', 'searchURL': 'http://www.bing.com/search?q=_searchTerms_'},
+    {'name': 'Demo Amazon.com', 'image': 'amazon', 'searchURL': 'http://www.amazon.com/exec/obidos/external-search/?field-keywords=_searchTerms_&mode=blended&tag=mozilla-20&sourceid=Mozilla-search'},
+    {'name': 'Demo eBay', 'image': 'ebay', 'searchURL': 'http://rover.ebay.com/rover/1/711-47294-18009-3/4?mpre=http://shop.ebay.com/?_nkw=_searchTerms_'},
+    {'name': 'Demo Twitter', 'image': 'twitter', 'searchURL': 'https://twitter.com/search?q=_searchTerms_&partner=Firefox&source=desktop-search'},
+    {'name': 'Demo Wikipedia (en)', 'image': 'wikipedia', 'searchURL': 'http://en.wikipedia.org/wiki/Special:Search?search=_searchTerms_&sourceid=Mozilla-search'}
   ]);
-
 
   /* Handle the list of recently-viewed sites. */
   var addSites = function addSites(links) {
