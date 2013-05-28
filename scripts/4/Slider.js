0f507882b1a2a4e2d784837124e8e7ff2814f3e8
Jack Montgomery <jackm321@gmail.com>
2013-05-28 10:09:54 -0700

    basic sliding done

--- scripts/4/Slider.js
+++ scripts/4/Slider.js
@@ -4,18 +4,111 @@
 
 
 ;(function(){
-  
+
   /* define the object */
   var Slider = function(){
-    var slider_el;
+
+    this.init();
+
+    /*
+    DATA
+     */
+    
+    this.panels = 3;
+    this.current_panel = 0;
+    /* number of pixels the slider div is currently shifted by */
+    this.current_shift = 0;
+
+    /* 
+    EVENTS
+    */
+   
+    /* window resize */
+    this.$els.window.resize(function() {
+      this.fix_size();
+    }.bind(this));
+
+    /* click prev button */
+    this.$els.prev_button.click(function() {
+      this.prev();
+    }.bind(this));
+
+    /* click next button */
+    this.$els.next_button.click(function() {
+      this.next();
+    }.bind(this));
+
   };
 
+  Slider.prototype.init = function(){
+    /* hide prev button */
+    $("#prev_button").hide();
+
+    /* find elements */
+    this.$els = {
+      prev_button: $("#prev_button"),
+      next_button: $("#next_button"),
+      slider_div: $("#slider_div"),
+      panels: $("#slider_div").children(),
+      header: $("#header"),
+      footer: $("#footer"),
+      window: $(window)
+    };
+
+    /* init sizes */
+    this.fix_size();
+
+  };
+
+
+
+
   Slider.prototype.prev = function(){
-    alert("prev");
+    // alert("prev");
+    this.current_panel--;
+    this.do_shift();
+
+    /* manage buttons */
+    this.$els.next_button.show();
+    if(this.current_panel === 0) this.$els.prev_button.hide();
   };
 
   Slider.prototype.next = function(){
-    alert("next");
+    // alert("next");
+    this.current_panel++;
+    this.do_shift();
+
+    /* manage buttons */
+    this.$els.prev_button.show();
+    if(this.current_panel === this.panels - 1) this.$els.next_button.hide();
+  };
+
+  /* looks at the current shift amount and updates
+  the css to reflect this */
+  Slider.prototype.do_shift = function(){
+    this.current_shift = this.current_panel * this.$els.window.width();
+    if (this.current_shift < 0) this.current_shift = 0;
+    this.$els.slider_div.css({
+      // 'transform': "translate3d(-"+this.current_shift+"px,0,0)"
+      'transform': "translate3d(-"+this.current_shift+"px,0,0)"
+    });
+  }
+
+  /*
+  this fixes the size of each panel to be exactly
+  the size of the browser window
+   */
+  Slider.prototype.fix_size = function(){
+    var height = this.$els.window.height() - this.$els.header.height() - this.$els.footer.height();
+    var width = this.$els.window.width();
+    
+    /* fix height */
+    this.$els.slider_div.css("height", height+"px");
+    /* fix width of each panel */
+    this.$els.panels.css("width", width+"px");
+  
+    /* fix shift */
+    this.do_shift();
   };
 
 
--- scripts/4/main.js
+++ scripts/4/main.js
@@ -4,6 +4,4 @@
 
 /* create a new slider */
 var slider = new Slider();
-
-/* regester the html element with it */
-slider.$el = $("#slider_div");
+// slider.init();
