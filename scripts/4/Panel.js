ead4e6ddf67894c38ffe7e14e3450f310a1aaca4
Jack Montgomery <jackm321@gmail.com>
2013-05-29 17:04:21 -0700

    basic panel sizing works

--- scripts/4/Panel.js
+++ scripts/4/Panel.js
@@ -10,10 +10,10 @@
       EVENTS
       */
 
-    /* window resize */
-    this.$els.window.resize(function(e) {
-      this.fix_size();
-    }.bind(this));
+      /* window resize */
+      this.$els.window.resize(function(e) {
+        this.fix_size();
+      }.bind(this));
 
     };
 
@@ -33,12 +33,41 @@
 
     Panel.prototype.fix_size = function(){
       var panel_height = this.$els.panel.height();
-      
+      var panel_width = this.$els.panel.width();
+      var apps_on_panel = this.$els.apps.length;
+      var denom;
+
+      /* calculates how wide the given app should
+      be given its container, the number of apps
+      it must share the container with, and it's
+      min_width factor */
+      function calc_app_width($app_container){
+        var min_size;
+        if($app_container.hasClass("app_3")){
+          min_size = 1;
+        }
+        else if($app_container.hasClass("app_2")){
+          min_size = 2;
+        }
+        else{
+          min_size = 3;
+        }
+        denom = Math.min(min_size, apps_on_panel);
+        return (panel_width-300)/denom;
+      }
+
       /* app_group */
       this.$els.app_group.css("height", panel_height-50+"px");
 
       /* apps */
       this.$els.apps.css("height", panel_height-100+"px");
+      this.$els.apps.each(function(i,app_container){
+        var $app_container = $(app_container);
+        var width = calc_app_width($app_container);
+        $(app_container).css("width", width+"px");
+      });
+
+
     };
 
     /* export */
