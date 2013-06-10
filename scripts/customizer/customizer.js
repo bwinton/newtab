babb5d539fbe8644bd8761f33cf8d4e2b38f796e
Jack Montgomery <jackm321@gmail.com>
2013-06-10 15:05:00 -0700

    stubbed customizer page

---
+++ scripts/customizer/customizer.js
@@ -0,0 +1,22 @@
+/**
+ * This is the code for the settings configuration tool for the #4 prototype
+ */
+
+;(function(){
+
+  function Customizer(){
+    this.init();
+  }
+
+  Customizer.prototype = {
+    init: function(){
+
+    }
+  };
+
+
+  /* run */
+  $(function(){
+    new Customizer();
+  });
+})();
