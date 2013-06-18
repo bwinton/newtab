63ecc5ff20d9e49e04dc1cddeb43dccf68fd2afa
Jack Montgomery <jackm321@gmail.com>
2013-06-18 08:12:57 -0700

    adding panels works now

--- scripts/customizer/customizer.js
+++ scripts/customizer/customizer.js
@@ -4,28 +4,26 @@
 
 ;(function(){
 
-  function Customizer(){
-    this.panels = [];
-    this.apps={};
-    this.data={};
+  function Customizer(apps_data){
+    this.data={
+      apps: [],
+      added_apps: [],
+      panels: []
+    };
 
-    /* should always be in the order that apps
-    exist on the newtab page */
-    this.data.added_apps = [];
-
-    this.init();
+    this.init(apps_data);
 
     /*
     Events
      */
 
-    $("button.add_app").click(function(e){
-      $target = $(e.target);
-      var $app_banner = $target.parent().parent();
-      var app_id = $.data($app_banner.get(0), 'app_id');
-      $app_banner.hide();
-      this.add_app(app_id);
-    }.bind(this));
+    // $("button.add_app").click(function(e){
+    //   $target = $(e.target);
+    //   var $app_banner = $target.parent().parent();
+    //   var app_id = $.data($app_banner.get(0), 'app_id');
+    //   $app_banner.hide();
+    //   this.add_app(app_id);
+    // }.bind(this));
 
     $("#new_panel_button")
     .on('mouseenter',function(e){
@@ -45,7 +43,7 @@
     /* scroll left or right */
 
     $(document).on('keydown',function(e){
-      $div = $("#slider_container")
+      $div = $("#slider_container");
       switch(e.keyCode){
         case(39): /* right */
           $div.scrollTo("+=480px", "easeOutElastic");
@@ -56,7 +54,7 @@
           $div.scrollTo("-=480px", "easeOutElastic");
           break;
       }
-    }.bind(this))
+    }.bind(this));
 
     /* prevent dragging certain elements on panels */
     $('.panel_header, .panel_footer, #new_panel_button').on('dragstart', prevent_drag);
@@ -65,276 +63,228 @@
     }.bind(this));
 
     /* app settings panel popup */
-    $(".app_container")
-    .on('mouseenter',function(e){
-      var $target = $(e.target);
-      var t = setTimeout(function() {
-        this.show_app_settings($(e.target));
-      }.bind(this), 1000);
-      $target.data('timeout', t);
-    }.bind(this))
-    .on('mouseleave',function(e){
-      $target = $(e.target).closest(".app_container");
-      clearTimeout($target.data('timeout'));
-    }.bind(this));
+    // $(".app_container")
+    // .on('mouseenter',function(e){
+    //   var $target = $(e.target);
+    //   var t = setTimeout(function() {
+    //     this.show_app_settings($(e.target));
+    //   }.bind(this), 1000);
+    //   $target.data('timeout', t);
+    // }.bind(this))
+    // .on('mouseleave',function(e){
+    //   $target = $(e.target).closest(".app_container");
+    //   clearTimeout($target.data('timeout'));
+    // }.bind(this));
 
 
 
   }
 
   Customizer.prototype = {
-    init: function(){
-      this.create_panel();
-      this.populate_apps_list(available_apps);
+    init: function(apps_data){
+      /* populate apps list */
+      $.each(apps_data, function(i, app_data){
+        this.create_app(app_data);
+      }.bind(this));
+
+      this.render_panels();
 
     },
 
     /*
+    handler functions
+    (setup data to call main functions)
+     */
+
+    /*
     main functions
      */
-    
+
     /* adds an app to newtab page */
     add_app: function(app){
-
+      this.insert_app(app);
+      this.optimize_array();
+      this.render_panels();
     },
 
     /* moves an app to a new spot on newtab page */
     move_app: function(app, new_index){
-
+      this.remove_app(app, true);
+      this.insert_app_at_index(app, new_index);
+      this.optimize_array();
+      this.render_panels();
     },
 
-    /* removes an app from newtab page */
-    remove_app: function(app){
+    /* removes an app from newtab page. 
+    if keep_size then doesn't delete the
+    apps size if fixed size is also true*/
+    remove_app: function(app, keep_size){
+      this.remove_app_from_active_apps(app);
+      this.optimize_array();
+      this.render_panels();
 
     },
 
     /* resizes an app on newtab page */
     resize_app: function(app, new_size){
+      app.size = new_size();
+      app.fixed_size = true;
+      this.optimize_array();
+      this.render_panels();
+    },
+
+    /* creates a new app and adds it to Customizer's
+    list of all apps */
+    create_app: function(data){
+      var app = new App(this, data);
+      this.data.apps.push(app);
+    },
+
+
+    /*
+    secondary functions
+     */
+
+    /* renders the Customizer's array of apps
+    into a series of panels */
+    render_panels: function(){
+      /* clear panels that are currently in dom */
+      $("#slider_div>.panel_wrapper").remove();
+      this.data.panels = [];
+
+      /* always create at least one panel, even if it's empty */
+      if(this.data.added_apps.length === 0) this.render_panel();
+      else{
+        var panels = this.generate_app_groups();
+        // alert(panels.length)
+        $.each(panels, function(i, panel){
+          this.render_panel.apply(this, panel.apps);
+        }.bind(this));
+      }
 
     },
 
-    /* adds extras to panels like header and footer */
-    decorate_panels: function(){
-      $(".slider_panel").each(function(i, panel){
-        $panel = $(panel);
-        /* add footers */
-        $('<div class="panel_footer">').appendTo(panel);
-        /* add headers */
-        $panel.append(generate_header);
-        // $('<div class="panel_header">').appendTo(panel);
+    /* takes any number of apps and renders a panel from them */
+    render_panel: function(){
+      console.log(arguments)
+      /* create panel */
+      var $panel = $("#templates .panel_wrapper").clone(true, true);
+      /* add to list of panels */
+      this.data.panels.push($panel);
+      /* set panel number */
+      $panel.find('.panel_number').html("Panel "+this.data.panels.length);
+
+      var $app_group = $panel.find('.app_group');
+
+
+      /* add apps to panel */
+      for(var x=0; x<arguments.length; x++){
+        var app = arguments[x];
+        var $app_container = app.$app_container
+        $app_container.removeClass('app_2 app_3 app_4 app_6');
+        console.log(app.size);
+        $app_container.addClass('app_'+ app.get_min_size());
+
+        $app_group.append($app_container);
+      }
+
+      /* add panel to the dom */
+      $("#new_panel_button").before($panel);
+      this.adjust_slider_width();
+
+    },
+
+    /* changes the size of the apps in the array so that
+    the size of apps are maximized while minimizing the
+    number of panels */
+    optimize_array: function(){
+
+    },
+
+    /* finds a spot in the array for the given
+    app and inserts it */
+    insert_app: function(app){
+      var optimal_spot = this.find_optimal_spot_for_app(app);
+      this.insert_app_at_index(app, optimal_spot);
+    },
+
+    /* inserts the given app at the given index */
+    insert_app_at_index: function(app, index){
+      this.data.added_apps.splice(index, 0, app);
+      // this.data.added_apps.push(app);
+    },
+
+    /*
+    helpers
+     */
+
+    find_optimal_spot_for_app: function(app){
+      var min_app_size = app.get_min_size();
+
+      var panels = this.generate_app_groups();
+      var best_panel;
+      $.each(panels, function(i, panel){
+        if(min_app_size + panel.min_size <= 6){
+          best_panel = panel;
+          return false;
+        }
+      });
+
+      /* find the app that the given app should come immediately after
+      or if none was found in panel search then pick the last app */
+      if(best_panel){
+        return this.data.added_apps.indexOf(best_panel.apps[best_panel.apps.length-1])+1;
+      }
+      else return this.data.added_apps.length;
+    },
+
+    /* returns a list of lists of apps from the
+    added_apps list where each list is the apps that
+    would fit on a panel */
+    generate_app_groups: function(){
+      var panels = [];
+
+      var current_panel;
+      $.each(this.data.added_apps, function(i, app){
+        var min_app_size = app.get_min_size();
+
+        /* create a new panel if one is needed */
+        if(!current_panel || current_panel.min_size + min_app_size > 6){
+          if(current_panel) panels.push(current_panel);
+          current_panel = {min_size: 0, apps: []};
+        }
+
+        /* add the current app to the panel */
+        current_panel.apps.push(app);
+        current_panel.min_size += min_app_size;
 
       });
-    },
+      if(current_panel) panels.push(current_panel);
 
-    /* finds a spot for the given app in the
-    array of apps and adds the app */
-    insert_app_into_array: function(){
-
+      return panels;
     },
 
     /* adjusts the overall width of the slider to accomidate all panels */
     adjust_slider_width: function(){
       var panel_width = 480;
       var panel_margin = 20;
-      var width = 110 + 10 + this.panels.length * (panel_width + panel_margin);
+      var width = 110 + 10 + this.data.panels.length * (panel_width + panel_margin);
       $('#slider_div').css('width', width+'px');
     },
 
-    create_panel: function(){
+    /* takes the array of apps and generates a JSON string
+    which represents the configuration as a series of panels
+    with apps on it */
+    generate_json: function(){
 
-      var $new_panel = $("#templates .panel_wrapper").clone(true, true);
-      var panel = new Panel(this, $new_panel);
-      this.panels.push(panel);
-
-      /* set panel number */
-      $new_panel.find('.panel_number').html("Panel "+this.panels.length);
-
-      $("#new_panel_button").before($new_panel);
-      this.adjust_slider_width();
-
-      return panel;
-    },
-
-    /* creates a new app in the list of available apps */
-    create_app: function(app_data){
-      console.log("creating app");
-      var $new_app = $("#templates .app_banner").clone(true, true);
-
-      /* fill in app details */
-      var lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, itaque, magnam, accusamus.";
-      
-      $new_app.find(".app_icon").attr('src', '../4/newtab_apps/Recently_Bookmarked/logo.png');
-      // $new_app.find(".app_icon").attr('src', 'http://www.google.com/images/srpr/logo4w.png');
-      
-      $new_app.find(".app_title").html(app_data.id);
-      $new_app.find(".app_description").html(lorem);
-
-      /* set jquery data of app_id onto the banner */
-      $.data($new_app.get(0), "app_id", app_data.id);
-
-      $("#apps_list").append($new_app);
-
-      /* add data to Customizier */
-      var app_data_copy = JSON.parse(JSON.stringify(app_data));
-      this.apps[app_data.id] = {
-        original_data: app_data_copy,
-        $banner: $new_app
-      };
-    },
-
-    /* adds a list of apps to the the list of available apps */
-    populate_apps_list: function(apps_list){
-      $(apps_list).each(function(i, app){
-        this.create_app(app);
-      }.bind(this));
-    },
-
-    /* adds an app to the new tab page */
-    add_app: function(app_id){
-      var app = this.apps[app_id];
-
-      var panel = this.find_or_create_panel(app);
-
-      panel.add_app(app);
-    },
-
-    find_or_create_panel: function(app){
-      /* try to find a panel with room for the app on it */
-      var return_panel;
-      $(this.panels).each(function(i, panel){
-        if(panel.room_for_app(app)){
-          return_panel = panel;
-          return false;
-        }
-      });
-      if(return_panel) return return_panel;
-
-      /* no panel found so create one */
-      else{
-        return this.create_panel();
-      }
-    },
-
-    show_app_settings: function($app_container){
-      if(!$app_container.hasClass('app_container')) return;
-      var $settings_panel = $("#templates .app_settings_panel").clone(true, true).html();
-      var app_id = $app_container.data('app_id');
-      var app = this.apps[app_id];
-
-      this.data.saved_app_contents = $app_container.html();
-      $app_container.html($settings_panel);
-      $app_container.addClass("settings_panel");
-
-      var $dropdown = $app_container.find(".size_selector");
-
-      /* setup app size dropdown */
-      $dropdown.find('option').filter(function(){
-        console.log("app.size: ", app.size)
-        console.log("this.val: ", $(this).val())
-        return $(this).val() === ""+app.size;
-      }).attr('selected', true);
-
-      /* setup close button */
-      $app_container.find("button.cancel_app_changes").click(function(e){
-        this.hide_app_settings($app_container);
-      }.bind(this));
-
-      /* setup save button */
-      $app_container.find("button.save_app_changes").click(function(e){
-        var new_size = parseInt($dropdown.val());
-        // alert(new_size);
-
-        if(new_size > 0) this.resize_app($app_container, new_size);
-        this.hide_app_settings($app_container);
-      }.bind(this));
-
-      /* setup remove_app button */
-      $app_container.find("button.remove_app").click(function(e){
-        var app_id = $app_container.data('app_id');
-        var app = this.apps[app_id];
-        app.panel.remove_app(app);
-      }.bind(this));
-
-    },
-
-    hide_app_settings: function($app_container){
-      console.log("exiting");
-      console.log($app_container);
-      var saved_html = this.data.saved_app_contents;
-      if(!saved_html){
-        return;
-      }
-      $app_container.html(saved_html);
-      $app_container.removeClass("settings_panel");
-      delete(this.data.saved_app_contents);
-    },
-
-    resize_app: function($app_container, size){
-      var app_id = $app_container.data('app_id');
-      var app = this.apps[app_id];
-
-      app.size = size;
-      app.fixed_size = true;
-
-      $app_container.removeClass('app_2 app_3 app_4 app_6');
-      $app_container.addClass('app_'+size);
-
-      this.reinsert_all_apps()
-    },
-
-    /* temporarily removes an app from its panel so that
-    it can be reinserted after a previous app is resized */
-    temp_remove_app: function(app){
-      /* remove app from panel */
-      app.$el.remove();
-      app.panel.apps.splice(app.panel.apps.indexOf(app), 1);
-      delete app.panel;
-
-      /* remove panel from app */
-      delete app.panel;
-      /* if size isn't fixed, remove size */
-      if(!app.fixed_size) delete app.size;
-    },
-
-    /* reinserts an app that was removed temporarily for
-    for resizing purposes */
-    reinstert_app: function(app){
-      /* find the correct panel for the app */
-      var panel = this.find_or_create_panel(app);
-
-      /* insert the app into the panel */
-      panel.$app_group.append(app.$el);
-
-      panel.apps.push(app);
-
-      panel.resize_apps();
-    },
-
-    /* temorarily removes and then reinserts all apps that are
-    currently on the newtab */
-    reinsert_all_apps: function(){
-      /* generate a list of all apps that are on newtab
-      and save them in order */
-      var apps_removed = [];
-      $.each(this.apps, function(i, app){
-        /* get only the apps that are on panels */
-        if(app.panel){
-          apps_removed.push(app);
-          this.temp_remove_app(app);
-        }
-      }.bind(this));
-
-      // alert("apps removed: "+apps_removed.length);
-
-      $.each(apps_removed, function(i, app){
-        this.reinstert_app(app);
-      }.bind(this));
     }
+
 
 
   };
 
-  function App(data){
+  function App(parent, data){
+    this.parent = parent;
     this.id = data.id;
     this.name = data.name;
     this.min_size = data.min_size;
@@ -345,9 +295,70 @@
 
   App.prototype = {
     init: function(){
+      this.create_banner();
+      this.create_app_container();
+    },
 
+    /*
+    main functions
+     */
+    
+    /* renders the app's banner, saves it and
+    adds it to the dom */
+    create_banner: function(){
+      var $banner = $("#templates .app_banner").clone(true, true);
+
+      /* fill in app details */
+      var lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, itaque, magnam, accusamus.";
+      
+      $banner.find(".app_icon").attr('src', '../4/newtab_apps/Recently_Bookmarked/logo.png');
+      
+      $banner.find(".app_title").html(this.name);
+      $banner.find(".app_description").html(lorem);
+
+      $("#apps_list").append($banner);
+
+      this.$banner = $banner;
+
+      /* setup events */
+
+      $banner.find('.add_app').click(function(){
+        this.add_app();
+      }.bind(this));
+    },
+
+    create_app_container: function(){
+      var $app_container = $("#templates>.app_container").clone(true, true);
+      $app_container.find(".app_container_content").html(this.id)
+      this.$app_container = $app_container;
+    },
+
+    /* adds app to active apps on newtab */
+    /* this calls the Customizer's add_app
+    method NOT vice versa */
+    add_app: function(){
+      this.parent.add_app(this);
+      this.hide_banner();
+    },
+
+    /*
+    secondary functions
+     */
+
+    get_min_size: function(){
+      if(this.fixed_size) return this.size;
+      else if(this.min_size) return this.min_size;
+      else return 2;
+    },
+
+    show_banner: function(){
+      this.$banner.show();
+    },
+
+    hide_banner: function(){
+      this.$banner.hide();
     }
-  }
+  };
 
   /* helpers */
 
@@ -367,6 +378,7 @@
 
   /* run */
   $(function(){
-    new Customizer();
+    new Customizer(available_apps);
   });
+
 })();
