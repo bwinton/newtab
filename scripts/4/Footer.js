4a60c99947d89ac34c9382cdb8a84fdecfce3af4
Jack Montgomery <jackm321@gmail.com>
2013-06-20 09:41:58 -0700

    set link to customizer

--- scripts/4/Footer.js
+++ scripts/4/Footer.js
@@ -1,6 +1,10 @@
 ;(function(){
     var Footer = function(){
 
+      /* go to customizer on click */
+      $('#settings').on('click', function(){
+        document.location.href = 'http://jackm321.github.io/newtab/customizer/index.html';
+      });
     };
 
     /* export */
