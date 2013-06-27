6d1b3f2659e41db91a536ba9006377c72c126f49
Jack Montgomery <jackm321@gmail.com>
2013-06-27 09:08:04 -0400

    added message passing for page switch to footer

--- scripts/4/Footer.js
+++ scripts/4/Footer.js
@@ -1,15 +1,22 @@
 ;(function(){
-    var Footer = function(){
+  var Footer = function(){
 
-      /* go to customizer on click */
-      $('#settings').on('click', function(e){
-        e.preventDefault();
-        e.stopPropagation();
-        document.location.href = 'about:newtab-config';
-      });
-    };
+    /* go to customizer on click */
+    $('#settings').on('click', function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      // document.location.href = 'about:newtab-config';
+      interesting('page-switch', 'newtab-config');
+    });
+  };
 
-    /* export */
-    window.Footer = Footer;
+  /* Send a message to the add-on. */
+  function interesting (type, detail) {
+    var event = new CustomEvent('tpemit', {'detail': {'type': type, 'detail': detail}});
+    document.dispatchEvent(event);
+  }
+
+  /* export */
+  window.Footer = Footer;
 
 })();
