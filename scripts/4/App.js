c310e8662fdddef715261b6d5514317b43b11023
Jack Montgomery <jackm321@gmail.com>
2013-06-27 09:26:14 -0400

    added listener for newtab layout in app.js

--- scripts/4/App.js
+++ scripts/4/App.js
@@ -11,24 +11,41 @@
     };
 
     NewTab.prototype.init = function(){
+        /* setup header and footer */
         this.submods.header = new Header();
         this.submods.footer = new Footer();
 
-        load_apps_data(function(apps_data){
-          this.submods.slider = new Slider(this, apps_data.panels);
-        }.bind(this));
+        /* listen for the apps_layout event */
+        window.addEventListener('message', function (event){
+          var data = event.data;
+
+          if (data.type === 'newtab-layout') {
+            this.submods.slider = new Slider(this, data.apps_layout);
+          }
+        });
+
+        /* emit init event */
+        interesting('initialized');
 
     };
 
 
     /* local helpers */
 
-    function load_apps_data(cb){
-      $.getJSON('./settings.json', function(data) {
-        cb(data);
-      });
+    // function load_apps_data(cb){
+    //   $.getJSON('./settings.json', function(data) {
+    //     cb(data);
+    //   });
 
-    }
+    // }
+
+    /* Send a message to the add-on. */
+    var interesting = function (type, detail) {
+      var event = new CustomEvent('tpemit', {'detail': {'type': type, 'detail': detail}});
+      console.log("emit message:");
+      console.log(type, detail);
+      document.dispatchEvent(event);
+    };
 
     /* run */
     $(function(){
